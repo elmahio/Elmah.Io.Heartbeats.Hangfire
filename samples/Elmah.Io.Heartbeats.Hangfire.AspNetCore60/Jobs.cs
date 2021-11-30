f88bfebeb5df01196b773443588b2babfbb8f528
Thomas Ardal <thomasardal@gmail.com>
2021-11-30 14:15:14 +0100

    Added ASP.NET Core 6.0 sample

---
+++ samples/Elmah.Io.Heartbeats.Hangfire.AspNetCore60/Jobs.cs
@@ -0,0 +1,11 @@
+﻿namespace Elmah.Io.Heartbeats.Hangfire.AspNetCore60
+{
+    public static class Jobs
+    {
+        [ElmahIoHeartbeat("API_KEY", "LOG_ID", "HEARTBEAT_ID")]
+        public static void Test()
+        {
+            Console.WriteLine("{0} Recurring job completed successfully!", DateTime.Now.ToString());
+        }
+    }
+}
---
+++ samples/Elmah.Io.Heartbeats.Hangfire.AspNetCore60/Program.cs
@@ -0,0 +1,41 @@
+using Elmah.Io.Heartbeats.Hangfire.AspNetCore60;
+using Hangfire;
+using Hangfire.MemoryStorage;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHangfire(config => config
+    // Remove the comment on the following line to install the elmah.io heartbeat filter as a global filter:
+    //.UseFilter(new ElmahIoHeartbeatAttribute(builder.Configuration["ElmahIo:ApiKey"], builder.Configuration["ElmahIo:LogId"], builder.Configuration["ElmahIo:HeartbeatId"]))
+    .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+    .UseSimpleAssemblyNameTypeSerializer()
+    .UseRecommendedSerializerSettings()
+    .UseMemoryStorage());
+builder.Services.AddHangfireServer();
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+var recurringJobManager = app.Services.GetService<IRecurringJobManager>();
+recurringJobManager.AddOrUpdate("Run every minute", () => Jobs.Test(), Cron.Minutely);
+
+app.Run();
